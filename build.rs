983807eb7efec51406067ee78f28f4b46f410af9
Brenda Po Lok Fahida <brenda.po@ui.ac.id>
2025-05-08 20:26:17 +0700

    Payment Service Implementation.

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .compile(
-            &["proto/service.proto"],   // Path to your proto file
+            &["proto/services.proto"],   // Path to your proto file
             &["proto"],               // Directory where the proto file is located
         )?;
     Ok(())
